8447fd42f44d81c9ad6f57216352e4e5f555a1dc
Khalid53 <a.khalekcse@gmail.com>
2019-08-14 17:51:26 +0300

    new update

--- js/myScript.js
+++ js/myScript.js
@@ -28,11 +28,52 @@ khalid.add();
 // 	} 
 // };
 // khalid.add();
-$('h1').html(function() {
+/*$('h1').html(function() {
 	$('h1').css({
 		'color' : 'red',
 		'font-size' : '36px',
 		'background-color' : 'green',
 
 	});
+} );*/
+	var firstScrollPosition = 0;
+ $('#scrollDiv').scroll( function() {
+ 	//alert('test');
+ 	//$('#h2').css('display', 'block').fadeOut(1000); for checking scroll event
+ 	var scrollPosition = $('#scrollDiv').scrollTop();
+ 	if (scrollPosition>firstScrollPosition){
+ 		$('#h2').text('Your are scrolling down');
+ 	} else {
+ 		$('#h2').text('Your are scrolling up');
+ 	}
+ 	firstScrollPosition = scrollPosition;
+
+ });
+
+
+
+
+$('#firstName').keyup( function(){
+	var firstName = $('#firstName').val();
+	$('#res1').text(firstName);
 } );
+
+$('#lastName').keyup( function() {
+	var lastName = $('#lastName').val();
+	$('#res2').text(lastName);
+});
+$('#lastName').blur( function() {
+	var firstName = $('#firstName').val();
+	var lastName = $('#lastName').val();
+	$('#res3').text(firstName+' '+lastName);
+});
+
+
+
+
+ // $('#btn').click( function (){
+// 	var firstName = $('#firstName').val();
+// 	var lastName = $('#lastName').val();
+// 	var fullName = firstName+' '+lastName;
+// 	$('#fullName').val(fullName);
+// } );
